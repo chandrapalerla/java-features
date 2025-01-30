4d1dd76070f70989a65518f308fb68bc115c7586
Chandrashekar Palerla <chandrashekar_palerla@epam.com>
2025-01-31 04:14:01 +0530

    remove .gitignore file all modules.

--- java-16/src/main/java/LocalVariableRecordInstance.java
+++ java-16/src/main/java/LocalVariableRecordInstance.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class LocalVariableRecordInstance {
 
     public static void main(String[] args) {
@@ -6,5 +9,14 @@ public class LocalVariableRecordInstance {
             System.out.println(x);
             System.out.println(y);
         }
+        String[] strings = {"flower", "flow", "flight"};
+        String prefix = strings[0];
+        for(int i=1;i<strings.length;i++){
+            while(strings[0].indexOf(prefix)!=0){
+                prefix = prefix.substring(0, prefix.length() -1);
+                }
+            }
+
+        System.out.println(prefix);
     }
 }
--- java-23/src/main/java/org/example/Main.java
+++ java-23/src/main/java/org/example/Main.java
@@ -3,5 +3,25 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int a[]  = {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9};
+        //output:1 9 8 4 2 7 6 9 0 0 0 0
+
+        int j=0;
+        for(int i = 0; i<a.length; i++){
+
+            if(a[i] != 0){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+
+                j++;
+            }
+
+        }
+
+        for (int a1 : a){
+            System.out.println(a1);
+        }
     }
 }
